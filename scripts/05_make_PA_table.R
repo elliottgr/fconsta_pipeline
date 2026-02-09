5c6c8dd9aa327e1d6655d8be7ec7b123ec13b673
Elliott <elliottgreene1@gmail.com>
2026-02-09 10:10:55 +0100

    updatede output script to save all tables (even the intermediary ones

--- scripts/05_make_PA_table.R
+++ scripts/05_make_PA_table.R
@@ -4,12 +4,21 @@
 args = commandArgs(trailingOnly=TRUE)
 ## handling missing filename
 if (length(args)==0){
-    filename<-paste("outputs/pa_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_pa<-paste("outputs/pa_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_filtered<-paste("outputs/filtered_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_pa_raw<-paste("outputs/pa_raw_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_ranked<-paste("outputs/ranked_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
 } else if (length(args)>=1) {
-    filename<-paste("outputs/", args[1], "_pa_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_pa<-paste("outputs/", args[1], "_pa_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_filtered<-paste("outputs/", args[1], "_filtered_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_pa_raw<-paste("outputs/", args[1], "_pa_raw_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
+    filename_ranked<-paste("outputs/", args[1], "_ranked_table", format(Sys.Date(),"_%Y-%b-%d"), ".csv",sep="")
 }
 
 
 source("scripts/04_filter_blast_results.R")
 results<-process_blast_hits(input="outputs/blast_outputs.tsv", min_pident = 70, min_qcovs = 50, annotation_file = NULL)
+write.csv(results$df_ranked, filename_ranked)
 write.csv(results$df_pa, filename)
+write.csv(results$df_filtered, filename_filtered)
+write.csv(results$df_pa_raw, filename_pa_raw)
